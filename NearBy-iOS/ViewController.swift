d2af985651151d636929a82f0dd0319c88d7c0bc
Mahmoud Amin <dr.mahmoud39@gmail.com>
2019-10-26 15:30:55 +0200

    Assign the views in the ViewController

--- NearBy-iOS/ViewController.swift
+++ NearBy-iOS/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var errorView: UIStackView!
+    
+    @IBOutlet weak var errorImage: UIImageView!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var retryButton: UIButton!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var venuesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        venuesTableView.dataSource = self
+        
+        errorView.isHidden = true
+        activityIndicator.isHidden = true
+        venuesTableView.isHidden = false
     }
-
-
+    
+    // TableView Implementation
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
