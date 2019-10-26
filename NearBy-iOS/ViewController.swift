6457c1ec18754daaa6073abd6176cb37d4e08753
Mahmoud Amin <dr.mahmoud39@gmail.com>
2019-10-26 12:20:16 +0200

    Initial Commit

---
+++ NearBy-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearBy-iOS
+//
+//  Created by Mahmoud Amin on 10/26/19.
+//  Copyright © 2019 Mahmoud Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
